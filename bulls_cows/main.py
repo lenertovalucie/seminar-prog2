091d561484e1f37f89d648395910f23af559a3fb
Havlíček Jiří <havlicek.jiri04@seznam.cz>
2022-11-01 21:02:08 +0100

    Prvni progres ukol

--- bulls_cows/main.py
+++ bulls_cows/main.py
@@ -1,1 +1,12 @@
-print("Konrad je impostor a Astralis jsou plebove")
+import random
+numbers = [1,2,3,4,5,6,7,8,9]
+secret_code = []
+
+while len(secret_code) < 5:
+   if random.choice(numbers) not in secret_code:
+        random.choice(numbers)
+
+print(secret_code)
+print("kamarad")
+
+
