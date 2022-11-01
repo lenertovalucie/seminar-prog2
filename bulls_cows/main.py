31599888277cfb9e9cb034d5e1c7bd3ff0ef47ce
Lukáš Foniok <lukyfoniok@gmail.com>
2022-11-01 21:04:53 +0100

    random numbers

--- bulls_cows/main.py
+++ bulls_cows/main.py
@@ -1,1 +1,15 @@
-print("ahojda")
+import random
+number = []
+number.append(random.randint(1,9))
+for n in range(3):
+    number.append(random.randint(0,9))
+print(number)
+
+answer = 0
+while answer == 0:
+    user = input("Answer:")
+    for n in range(len(answer)):
+        
+    
+
+
