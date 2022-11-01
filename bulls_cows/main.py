0a1b28fc742dd7ff8caa09b6405786098d88a084
Havlíček Jiří <havlicek.jiri04@seznam.cz>
2022-11-01 22:29:50 +0100

    Finalni progres ukol

--- bulls_cows/main.py
+++ bulls_cows/main.py
@@ -1,12 +1,33 @@
 import random
-numbers = [1,2,3,4,5,6,7,8,9]
+numbers = ["1","2","3","4","5","6","7","8","9"]
 secret_code = []
-
-while len(secret_code) < 5:
-   if random.choice(numbers) not in secret_code:
-        random.choice(numbers)
-
-print(secret_code)
-print("kamarad")
+while len(secret_code) < 4:
+  secret_code.append(random.choice(numbers))
+  numbers.remove(secret_code[-1])
+secret_code_string = secret_code[0] + secret_code[1] + secret_code[2] + secret_code[3]
 
 
+guess = input("Try to guess the code: ")
+cows = 0
+bulls = 0
+while guess != secret_code_string:
+  cows = 0
+  bulls = 0
+  for i in range (0,len(guess)):
+    if guess[i] == secret_code[i]:
+      bulls += 1
+    elif guess[i] in secret_code:
+      cows += 1
+    else:
+      None  
+  if bulls == 1:
+    bulls = "1 Bull"
+  else:
+    bulls = str(bulls) + " Bulls"
+  if cows == 1:
+    cows = "1 Cow"
+  else:
+    cows = str(cows) + " Cows"
+  print(f"{bulls} and {cows}")
+  guess = input("Guess: ")
+print("You cracked the code")
