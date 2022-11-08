53a85c11c1b474a6127c3cbd128fe9926211fb0c
balcarma <matyasbalcar0@gmail.com>
2022-11-08 19:05:00 +0100

    adde loops and tested

--- bulls_cows/main.py
+++ bulls_cows/main.py
@@ -1,3 +1,5 @@
+running=True
+
 
 import random
 def generate_code(delka,minimum,maximum):
@@ -11,29 +13,30 @@ def get_player_code():
     return kod
 
 correct_code=generate_code(4,1,8)
-print(correct_code)
+#?print(correct_code)
+while running:
+    player_code=get_player_code()
+    bulls=0
+    cows=0
+    for i in range(len(player_code)):
+        if correct_code[i]==player_code[i]:
+            bulls+=1
 
-player_code=get_player_code()
-bulls=0
-cows=0
-for i in range(len(player_code)):
-    if correct_code[i]==player_code[i]:
-        bulls+=1
-
-no_dupe_p_code="";
-for i in range(0,len(player_code)):
-    if player_code[i] not in no_dupe_p_code:
-        no_dupe_p_code+=player_code[i]
+    no_dupe_p_code="";
+    for i in range(0,len(player_code)):
+        if player_code[i] not in no_dupe_p_code:
+            no_dupe_p_code+=player_code[i]
+        else:
+            pass
+    for i in range(0,len(no_dupe_p_code)):
+        if no_dupe_p_code[i] in correct_code:
+            cows+=1
+    cows=cows-bulls
+    if player_code==correct_code:
+        print("you won")
+        running=False
     else:
-        pass
-for i in range(0,len(no_dupe_p_code)):
-    if no_dupe_p_code[i] in correct_code:
-        cows+=1
-cows=cows-bulls
-if player_code==correct_code:
-    print("you won")
-else:
-    print(f"Bulls: {bulls}")
-    print(f"Cows {cows}")
-    print("try again: ")
+        print(f"Bulls: {bulls}")
+        print(f"Cows {cows}")
+        print("try again: ")
 
