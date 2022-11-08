3ab8fd3be8c235ea98dd3db5d54801a577c98db5
Milan Kolář <minosek.kolar@gmail.com>
2022-11-08 18:55:40 +0100

    Funkční, bez testů vstupu

--- bulls_cows/main.py
+++ bulls_cows/main.py
@@ -12,12 +12,31 @@ numbers.remove(sec2)
 sec3 = numbers[random.randint(0,len(numbers)-1)]
 
 secret = sec0*1000 + sec1*100 + sec2*10 + sec3
-
-print("This is a guessing game! Don\'t hesitate and guess!")
-guess = input("Guess: ")
-guess_lst = []
-for i in str(guess):
-    guess_lst.append(i)
+secret_lst = []
+for i in str(secret):
+    secret_lst.append(i)
+print(secret)
 
 bull = 0
-cow = 0
+print("This is a guessing game! Don\'t hesitate and guess!")
+
+while bull != 4:
+    print("Guess: ", end="")
+    guess = input()
+    guess_lst = []
+    for i in str(guess):
+        guess_lst.append(i)
+
+    bull = 0
+    cow = 0
+
+    for i in range(4):
+        if guess_lst[i] == secret_lst[i]:
+            bull += 1
+        else:
+            for m in range(4):
+                if guess_lst[i] == secret_lst[m]:
+                    cow += 1
+    print(f"Response: {bull} bulls, {cow} cows")
+
+print("You guessed right!")
