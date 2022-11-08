f4d6619fd8571e667902da6ceb047388d0054934
Milan Kolář <minosek.kolar@gmail.com>
2022-11-08 18:16:54 +0100

    Generátor čísel

--- bulls_cows/main.py
+++ bulls_cows/main.py
@@ -1,27 +1,14 @@
 import random
 
-# secret = str(random.randint(1000, 9999))
-# sec0 = int(secret[0])
-# sec1 = int(secret[1])
-# sec2 = int(secret[2])
-# sec3 = int(secret[3])
-
-# if sec0 == sec1 or sec2 or sec3:
-#     secret = str(random.randint(1000, 9999))
-#     sec0 = int(secret[0])
-#     sec1 = int(secret[1])
-#     sec2 = int(secret[2])
-#     sec3 = int(secret[3])
-
-sec0 = random.randint(1,9)
-sec1 = sec0
-while sec1 != sec0:
-    sec1 = random.randint(0,9)
-sec2 = sec1
-while sec2 != sec1 or sec2 == sec0:
-    sec2 = random.randint(0,9)
-sec3 = sec2
-while sec3 != sec2 or sec3 == sec1 or sec3 == sec0:
-    sec3 == random.randint(0,9)
+numbers = [0,1,2,3,4,5,6,7,8,9]
+sec0 = 0
+while sec0 == 0:
+    sec0 = numbers[random.randint(0,9)]
+numbers.remove(sec0)
+sec1 = numbers[random.randint(0,len(numbers)-1)]
+numbers.remove(sec1)
+sec2 = numbers[random.randint(0,len(numbers)-1)]
+numbers.remove(sec2)
+sec3 = numbers[random.randint(0,len(numbers)-1)]
 
 print(sec0, sec1, sec2, sec3)
